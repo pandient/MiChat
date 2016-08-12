781a5c929c446ad607025d9dc89b8c64152f5b7d
Tony Lei <tltonylei@gmail.com>
2016-08-11 17:07:58 -0700

    add docker container environment checks

--- server/loginservice/server.js
+++ server/loginservice/server.js
@@ -33,7 +33,12 @@ const http = require("http");
 const os = require('os');
 
 var ifaces = os.networkInterfaces();
+var serviceRegistryIP = "10.0.9.30";
+var serviceRegistryPort = 3001;
 var hostIP;
+var hostPort = 3002; //set to null or some hardcoded value, but should get from container env
+
+
 
 Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
@@ -46,10 +51,10 @@ Object.keys(ifaces).forEach(function (ifname) {
 
     if (alias >= 1) {
       // this single interface has multiple ipv4 addresses
-      //console.log(ifname + ':' + alias, iface.address);
+      // console.log(ifname + ':' + alias, iface.address);
     } else {
       // this interface has only one ipv4 adress
-      //console.log(ifname, iface.address);
+      // console.log(ifname, iface.address);
 	  if (ifname.indexOf('Local') > -1) {
 		hostIP = iface.address;
 	  }
@@ -57,29 +62,40 @@ Object.keys(ifaces).forEach(function (ifname) {
     ++alias;
   });
 });
+console.log(process.env.servicePort);
+// checking container env for service info to pass to service registry
+hostPort = process.env.servicePort? process.env.servicePort : hostPort;
+hostIP = process.env.serviceIP? process.env.serviceIP : hostIP;
+console.log(hostPort + " " + hostIP);
+
 
 var postData = querystring.stringify({
 	"name": "loginservices",
 	"host": hostIP,
-	"port": "3002"
+	"port": hostPort
 });
 //options include hostname and port to service registry
-var options = {
-	hostname: "10.0.9.30",
-	port: 3001,
-	method: "POST",
-	path: "/add",
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'Content-Length': Buffer.byteLength(postData)
-	}
-};
-var req = http.request(options, function(response) {
-	console.log("ok, i've told the service registry");
-});
+if (hostIP && hostPort) {
+	var options = {
+		hostname: serviceRegistryIP,
+		port: serviceRegistryPort,
+		method: "POST",
+		path: "/add",
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': Buffer.byteLength(postData)
+		}
+	};
+	var req = http.request(options, function(response) {
+		console.log("ok, i've told the service registry");
+	});
 
-req.on('error', function(err) {
-	console.log("problem with adding to service registry");
-});
-req.write(postData);
-req.end();
+	req.on('error', function(err) {
+		console.log("problem with adding to service registry");
+	});
+	req.write(postData);
+	req.end();
+}
+else {
+	console.log("failed to locate host information to send to service registry");
+}
