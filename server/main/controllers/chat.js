70aae70f0efe99e83a66952d49a197601f2ec1c3
SMax <you@abc.com>
2016-08-12 09:40:13 -0700

    added chat

--- server/main/controllers/chat.js
+++ server/main/controllers/chat.js
@@ -1,10 +1,15 @@
 
+var db = require('../db');
 
 
 exports.sendMessage = (req, res) => {
   // microservice
   // repond
-  res.json( {
+  var user = req.body.user;
+  var message = req.body.message;
+  db.sendMessage(user,message);
+
+  res.json({
     message: 'messagerecieved'
   });
 };
@@ -12,14 +17,20 @@ exports.sendMessage = (req, res) => {
 exports.getMessages = (req, res) => {
   // microservice
   // repond
-  res.json( {
-    messages: ["1"]
-  });
+  var callback = function (data) {
+    console.log(data);
+    res.json({
+      data
+    });
+  }
+
+  var messages = db.getMessages("asdf", callback);
+
 };
 
 exports.listOfRooms = (req, res) => {
   // look in db for list of rooms - hardcoded
-  res.json( {
-    roomList: ["DDX","ZEMA","DM","Support","Data Delivery"]
+  res.json({
+    roomList: ["DDX", "ZEMA", "DM", "Support", "Data Delivery"]
   });
 };
--- server/main/server.js
+++ server/main/server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+var mongoose = require('mongoose');
+var db = require('./db');
 
 const apiController = require('./controllers/api');
 const loginController = require('./controllers/login');
 const chatController = require('./controllers/chat');
-
+db.setup();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,11 +23,11 @@ app.get('/listofrooms', chatController.listOfRooms);
 app.post('/sendMessage', chatController.sendMessage);
 app.get('/getMessages', chatController.getMessages)
 
-app.listen(3000, function() {
+app.listen(3001, function () {
   console.log('main service listening on 3000')
 })
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('this is the main service listing')
 })
 
